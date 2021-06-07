e86b7d0c014b4e2c1bc947e1e00a3055b8bb38b1
Antonio Montero <antonio.montero@gmail.com>
2021-06-07 17:22:31 -0500

    feat(api/database/migrations): add tables

--- api/src/database/migrations/20210607145625-initial_migration.js
+++ api/src/database/migrations/20210607145625-initial_migration.js
@@ -55,7 +55,8 @@ module.exports = {
         references: {
           model: 'product',
           key: 'id'
-        }
+        },
+        allowNull: false
       },
       createdAt: {
         type: Sequelize.DATE,
@@ -119,9 +120,233 @@ module.exports = {
         allowNull: true
       },
     })
+
+    await queryInterface.createTable('customer', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    })
+
+    await queryInterface.createTable('order', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      customerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'customer',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM("created", "in_process", "cancelled", "completed"),
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      total: {
+        type: Sequelize.FLOAT(10, 2)
+      }
+    })
+
+    await queryInterface.createTable('order_item', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      ProductId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      OrderId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'order',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      subtotal: {
+        type: Sequelize.FLOAT(10, 2)
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      }, 
+    })
+
+    await queryInterface.createTable('review', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      customerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'customer',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      score: {
+        type: Sequelize.FLOAT(2, 1)
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    })
+
+    await queryInterface.createTable('shipping_address', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      zip: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      neighborhood: {
+        type: Sequelize.STRING,
+      },
+      customerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'customer',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    }) 
+
+    await queryInterface.createTable('user', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.ENUM("admin", "staff"),
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user');
+    await queryInterface.dropTable('shipping_address');
+    await queryInterface.dropTable('review');
+    await queryInterface.dropTable('order_item');
+    await queryInterface.dropTable('order');
+    await queryInterface.dropTable('customer');
     await queryInterface.dropTable('product_category');
     await queryInterface.dropTable('category');
     await queryInterface.dropTable('image');

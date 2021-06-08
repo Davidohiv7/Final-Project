145f4b07ed062f4061775eabd25f664d29ce3e7a
Your NameDavid <davidbu7@hotmail.com>
2021-06-08 14:50:56 -0500

    Product card with image, title and score

--- client/src/components/ProductCards/ProductCards.jsx
+++ client/src/components/ProductCards/ProductCards.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useStyles from './ProductsCardsStyles'
-import { Card } from '@material-ui/core';
+import { Card, CardMedia, Typography, CardContent, Box } from '@material-ui/core';
+import StarIcon from '@material-ui/icons/Star';
 
 function ProductCards() {
 
     const classes = useStyles();
 
+    const [scoreArray, setScoreArray] = useState([]);
+
+    const productExample = {
+        name: 'Pansito',
+        image: 'https://assets.bonappetit.com/photos/5f84743360f032defe1f5376/16:9/w_2560%2Cc_limit/Pullman-Loaf-Lede-new.jpg',
+        score: 5,
+        price: 5,
+    }
+
+    function createArrayFromNumber(number) {
+        const arrayOfNumbers = []
+            for (let i = 1; i <= number; i++) {
+                arrayOfNumbers.push(i);
+            }
+        return arrayOfNumbers     
+    }
+
+    useEffect(() => {
+        const newScoreArray = createArrayFromNumber(productExample.score)
+        setScoreArray(newScoreArray)
+    })
+
     return (
-        <div>
-            Product cards component
-        </div>
+        <Card className={classes.body}>
+            <CardMedia
+                className={classes.image}
+                title={productExample.name}
+                image={productExample.image}
+            />
+            <CardContent>
+                <Typography align='center' variant="h6" color='secondary' >{productExample.name}</Typography>
+                <Box display="flex" justifyContent="center">
+                    {
+                        scoreArray.map(() => <StarIcon color='inherit' />) 
+                    }
+                </Box>
+            </CardContent>
+        </Card>
     );
 }
 
--- client/src/components/ProductCards/ProductsCardsStyles.js
+++ client/src/components/ProductCards/ProductsCardsStyles.js
@@ -2,8 +2,14 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     body: {
-      background: 'black',
+      width: 220,
+      height: 300,
+      backgroundColor: '#CE1212',
+      borderRadius: 15,
     },
+    image: {
+        height: 150,
+    }
   });
 
 

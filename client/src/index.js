d91f6873ac846e74cd658d3057139e4b052403f5
MarianoBaeza <marianobaeza@hotmail.com>
2021-06-08 13:58:56 -0300

    MaterialUI Palette and Typography changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
@@ -9,11 +11,28 @@ import store from './store/store'
 import './index.css';
 import App from './App';
 
+const theme = createMuiTheme({
+  palette: {
+    // LightRed - DarkRed
+    primary: {
+      main: '#CE1212',
+      dark: '#810000'
+    },
+    // LightGray - DarkGray
+    secondary: {
+      main: '#EEEBDD',
+      dark: '#1B1717'
+    }
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,

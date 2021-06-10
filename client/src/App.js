71f986077df7a634d157481a47b81c8a2a904982
David Vivas <davidbu7@hotmail.com>
2021-06-10 13:15:00 -0500

    home corrected

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import NavBar from './Views/NavBar/NavBar'
 import Home from './Views/Home/Home.jsx'
 import { ThemeProvider } from '@material-ui/core/styles'
-import theme from './themeConfig'
+import theme from './themeConfig.js'
 
-import theme from './themeConfig.js';
-
-import NavBar from './components/Navbar/NavBar'
-import ProductCards from './components/ProductCards/ProductCards'
-import { Catalogue } from './components/Catalogue/Catalogue.jsx';
 
 export default function App() {
   return (
   	<ThemeProvider theme = {theme}>
-      <NavBar/>
-      <Catalogue/>
+      <div>Home</div>
   	</ThemeProvider>
   );
 }

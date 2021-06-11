0d59c22ebfec300717ad17b47e3a12a96cc2d2f3
David Vivas <davidbu7@hotmail.com>
2021-06-11 17:46:24 -0500

    local JWT authentication working

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 const product = require('./product')
+const signup = require('./signup')
+const signin = require('./signin')
+const test = require('./test')
 
 router.use('/products', product);
+router.use('/signup', signup);
+router.use('/signin', signin);
+router.use('/test', test);
 
 module.exports = router;
---
+++ api/src/routes/signin.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const response = require('../utils/response')
+
+const signInRouter = express.Router();
+
+const { SECRET_KEY_JWT } = process.env
+
+
+const models = require('../database/models/');
+
+signInRouter.get('/', (req, res, next) => {
+    res.send('Esta es la ruta de sign in')
+})
+  
+signInRouter.post('/', async (req, res, next) => {
+
+    const { email, password} = req.body
+
+    const userExistCheck = await models.User.findOne({ where: { email }})
+    if(!userExistCheck) {
+        return response.success(req, res, { message: 'The email or the password does not match' })
+    }
+
+    const match = await bcrypt.compare(password, userExistCheck.password);
+
+    if(!match) {
+        return response.success(req, res, { message: 'The email or the password does not match' })
+    }
+
+    const jasonWebToken = jwt.sign({id: userExistCheck.id, username: userExistCheck.username}, SECRET_KEY_JWT)
+    return response.success(req, res, {message: 'Successful log in', token: jasonWebToken})
+})
+
+module.exports = signInRouter

84258d6b399876a6397472c1d88b6edddcb045ab
Federico Leon <fede.damian.leon@gmail.com>
2021-06-24 04:00:32 -0300

    verify key API endpoint

--- api/src/routes/passwordReset.js
+++ api/src/routes/passwordReset.js
@@ -2,30 +2,46 @@ const router = require('express').Router();
 const { authMailing, transporter } = require('../mailingMid/NodemailerGoogleMid');
 const jwt = require('jsonwebtoken');
 const models = require('../database/models');
+const response = require('../utils/response');
 
-const { SECRET_KEY_JWT } = process.env;
+const { SECRET_KEY_JWT, GOOGLE_MAIL } = process.env;
 
 router.post('/getKey', async (req, res, next) => {
     const { email } = req.body;
     try {
         const person = await models.Person.findOne({ where: { email }});
-        console.log(person);
         if(!person) {
-            res.send('success');
+            response.success(null, res);
         } else {
-            const { id, email } = user;
+            const { id, email } = person;
             const token = jwt.sign({ id, email }, SECRET_KEY_JWT, { expiresIn: 300 });
             transporter.sendMail({
                 from: `Onion Food Sup. <${GOOGLE_MAIL}>`,
-                to: user.email,
+                to: person.email,
                 subject: 'Password reset requested',
-                html: `<a href='http://localhost:3000/resetPassword?token=${token}'>Reset password</a>`,
+                html: `<a href='http://localhost:3000/passwordReset?token=${token}'>Reset password</a>`,
                 auth: authMailing
             });
-            res.send('success');
+            response.success(null, res);
         }
     } catch (error) {
-        console.error(error);
+        response.error(null, res);
+    }
+});
+
+router.post('/verifyKey', (req, res, next) => {
+    const { token } = req.body;
+    if(!token) {
+        response.error(null, res, 'Not token supplied', 404);
+    } else {
+        jwt.verify(token, SECRET_KEY_JWT, function(error, data) {
+            if(!error) {
+                response.success(null, res, data);
+            } else {
+                console.log(error);
+                response.error(null, res, error);
+            }
+        });
     }
 });
 

b1662216a9b659a784f33e67d83ae126eed80f5d
Pablo <basilipablo@gmail.com>
2021-06-30 14:05:22 -0300

    Nico made all the changes to be Responsive, Im just uploading due to an error in his GitBash.

--- client/src/Views/Authentication/styles.js
+++ client/src/Views/Authentication/styles.js
@@ -1,19 +1,30 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        borderRadius: theme.shape.borderRadius,
-    },
-    container: {
-        width: 750,
-        height: 500,
-        backgroundColor: theme.palette.common.white,
-        borderRadius: theme.shape.borderRadius,
-      },
+  root: {
+    borderRadius: theme.shape.borderRadius,
 
-    height100: {
-        height: '100%',
+    [theme.breakpoints.down("xs")]: {
+      backgroundColor: "white",
     },
+  },
+
+  container: {
+    width: 750,
+    height: 500,
+    backgroundColor: theme.palette.common.white,
+    borderRadius: theme.shape.borderRadius,
+  },
+
+  height100: {
+    height: "100%",
+  },
+  heightCardMedia: {
+    height: "100%",
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
+  },
 }));
 
 export default useStyles;
